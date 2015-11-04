e724d028f5f113040bd9df2d5514567fc95c8090
Shashank Murthy <ShashankMurthy@Shashanks-MacBook-Pro.local>
2015-11-03 22:03:11 -0600

    Stuff

--- HW2/src/hw2/GameUtil.java
+++ HW2/src/hw2/GameUtil.java
@@ -6,19 +6,39 @@ import api.Move;
 
 public class GameUtil {
 
+	private Move useMove;
+	
 	public GameUtil(){
 		
 	}
 	
 	public static int findNextNonemptyCell(int[] arr,int start){
-		
-		
-		return start;
+
+		for(int i =start;i<arr.length;i++){
+			if(!(arr[i]==0)){
+				return i;
+			}
+		}
+		return -1;
 		
 	}
 	
 	public static Move findNextMove(int[] arr, int index){
 		
+		for(int i =index;i<arr.length;i++){
+			if(!(arr[i] == 0)){
+				for(int a:arr){
+					if(!(a==0)){
+						if(a==arr[i]){
+							return 
+						}
+					}
+				}
+			}
+			else{
+				
+			}
+		}
 		
 		return null;
 		
@@ -32,17 +52,7 @@ public class GameUtil {
 	}
 	
 	public static ArrayList<Move> collapseArray(int[] arr){
-		ArrayList<Integer> tempArray = new ArrayList<Integer>(arr.length);
-		int i =0;
-		for(int a: arr){
-			i ++;
-			if(!(a==0)){
-				tempArray.add(a);
-			}
-			else{
-				tempArray.add(arr.length - i, a);
-			}
-		}
+		
 		
 		return null;
 		
