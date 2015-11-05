e4c7742d232d4f72b8e2b3621ff9fdd8e73d2ea6
Shashank Murthy <ShashankMurthy@Shashanks-MacBook-Pro.local>
2015-11-04 23:39:07 -0600

    BLAH BLAH2

--- HW2/src/hw2/GameUtil.java
+++ HW2/src/hw2/GameUtil.java
@@ -6,6 +6,23 @@ import api.Move;
 
 public class GameUtil {
 
+	public static void main(String[] args){
+		
+		int[] arr = new int[5];
+		
+		arr[0] =4;
+		arr[1] =2;
+		arr[2] =0;
+		arr[3] =2;
+		arr[4] =0;
+		
+		int index1 = findNextNonemptyCell(arr,1);
+		int index2 = findNextNonemptyCell(arr,index1);
+		
+		System.out.println(index2);
+		
+	}
+	
 	private static Move useMove;
 	private static int[] temparr;
 	
@@ -14,14 +31,12 @@ public class GameUtil {
 	}
 	
 	public static int findNextNonemptyCell(int[] arr,int start){
-
-		for(int i =start;i<arr.length;i++){
-			if(!(arr[i]==0)){
-				return i;
+		for(int i = start+1;i<arr.length;i++){
+			if(arr[i] != 0){
+				return i-1;
 			}
 		}
 		return -1;
-		
 	}
 
 	public static Move findNextMove(int[] arr, int index){
@@ -30,12 +45,15 @@ public class GameUtil {
 		int index1 = findNextNonemptyCell(arr,index);
 		int index2 = findNextNonemptyCell(arr,index1);
 		
+		//System.out.println(index1);
+		//System.out.println(index2);
 		
-
-		if(arr[index1] == arr[index2] && index1 != -1 && index2 != -1){
+		if(arr[index1] == arr[index2] &&index2 != -1){
+			System.out.println("BRUH BRUH");
 			return new Move(index1,index2,index,arr[index1]);
 		}
 		else{
+			System.out.println("LOL LOL");
 			return new Move(index1,index,arr[index1]);
 		}
 		
